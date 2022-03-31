4c16171a2dba2bc34a1e33b65f426dbd69629ab3
yuseipen0716 <yuseipen1@gmail.com>
2022-03-31 17:25:48 +0900

    seeds.rbにてデフォルト値の準備

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# もしもdeviseでadminユーザのデフォルト値が使いたくなったら、以下のコメントアウトを解除
+# admin = Admin.new(:email => 'admindummy@', :password => 'adminpass')
+# admin.save!
+
