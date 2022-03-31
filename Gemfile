c05fc6afae5571cad2944d15713bb6b9f48dc113
yuseipen0716 <yuseipen1@gmail.com>
2022-03-31 15:29:01 +0900

    devise Getting startedを参考にdeviseを使う準備

--- Gemfile
+++ Gemfile
@@ -24,6 +24,9 @@ gem 'jbuilder', '~> 2.7'
 # gem 'image_processing', '~> 1.2'
 gem 'hamlit-rails', '~> 0.2.3'
 
+# deviseを導入
+gem 'devise'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # devise set upの際に追加
+  config.action_mailer.defaul_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
