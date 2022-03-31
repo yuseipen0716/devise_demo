bca18a475d05afc31aa7f690f8fa614b63acc0b5
yuseipen0716 <yuseipen1@gmail.com>
2022-03-31 16:59:50 +0900

    articlesモデル　migration

--- db/migrate/20220331063334_create_articles.rb
+++ db/migrate/20220331063334_create_articles.rb
@@ -1,8 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
-      t.string :title
-      t.text :content
+      t.string :title, null: false
+      t.text :content, null: false
 
       t.timestamps
     end
