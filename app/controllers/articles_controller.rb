7054ab8dbb20fb03fcee291248c6090f377d3366
m_makino <m_makino@upacity.jp>
2022-04-04 14:10:36 +0900

    new action/ create action implement

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,34 @@
 class ArticlesController < ApplicationController
     def index
+      @articles = Article.all
     end
     
     def new
+      @article = Article.new
+    end
+
+    def create
+      @article = Article.new(article_params)
+
+      respond_to do |format|
+        if @article.save
+          format.html { redirect_to article_url(@article), notice: "記事を新規作成しました" }
+          format.json { render :show, status: :created, location: @article }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @article.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    private
+    # Use callbacks to share common setup or constaraints berween actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def article_params
+      params.require(:article).permit(:title, :content)
     end
 end
