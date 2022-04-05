3e4b08766042a5e2cf35ef2fd6aa184dbb257b96
m_makino <m_makino@upacity.jp>
2022-04-05 16:44:52 +0900

    deviseのcontrollerが使えるようにroutingを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins, :controllers => {
+    :confirmations => 'admins/confirmations',
+    :registrations => 'admins/registrations',
+    :sessions => 'admins/sessions',
+    :password => 'admins/passwords'
+  }
+
   resources :articles
-  devise_for :admins
+
   root "articles#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
