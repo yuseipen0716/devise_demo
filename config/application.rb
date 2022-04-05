32c2b66140f50f2a6472debb786ef8a023bad954
m_makino <m_makino@upacity.jp>
2022-04-05 16:43:43 +0900

    deviseを日本語化

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module DeviseDemo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # deviseを日本語化
+    config.i18n.default_locale = :ja
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
