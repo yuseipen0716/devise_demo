49e6101843af907e6730b3ae976cfb236bddf98a
m_makino <m_makino@upacity.jp>
2022-04-06 12:10:41 +0900

    add category_id to Article_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_31_063334) do
+ActiveRecord::Schema.define(version: 2022_04_06_030523) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_03_31_063334) do
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
 end
