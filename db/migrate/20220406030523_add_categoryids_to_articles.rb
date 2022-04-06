c44da4e5f5d85fc5f54c321c623b2bccb6cbbd59
m_makino <m_makino@upacity.jp>
2022-04-06 12:57:08 +0900

    モデルの作成やリレーション

--- db/migrate/20220406030523_add_categoryids_to_articles.rb
+++ db/migrate/20220406030523_add_categoryids_to_articles.rb
@@ -1,5 +1,5 @@
 class AddCategoryidsToArticles < ActiveRecord::Migration[6.1]
   def change
-    add_column :articles, :category_id, :integer
+    add_column :articles, :category_id, :integer, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_032401) do
+ActiveRecord::Schema.define(version: 2022_04_06_032811) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,9 +33,20 @@ ActiveRecord::Schema.define(version: 2022_04_06_032401) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "categorizations", force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_categorizations_on_article_id"
+    t.index ["category_id"], name: "index_categorizations_on_category_id"
+  end
+
+  add_foreign_key "categorizations", "articles"
+  add_foreign_key "categorizations", "categories"
 end
