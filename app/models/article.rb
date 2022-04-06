a69afabfc2ade9e2f80ae406cc78c79120fff1cf
m_makino <m_makino@upacity.jp>
2022-04-06 19:03:27 +0900

    各modelにdependent_destroyを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
-  has_many :categorizations
+  has_many :categorizations, dependent: :destroy
+  has_many :categories, through: :categorizations
   validates :title, length: { maximum: 100 }, presence: true
   validates :content, length: { maximum: 100000 }, presence: true
 end
