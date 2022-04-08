69986bdaf6541bcd0b3e23ddc5dd8a1752bc58be
m_makino <m_makino@upacity.jp>
2022-04-09 01:42:01 +0900

    [fix]articles#index descに

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 
   
     def index
-      @articles = Article.all.page(params[:page]).per(10)
+      @articles = Article.all.order(created_at: :desc).page(params[:page]).per(10)
       @categories = Category.all
       @archives = @articles.group("strftime('%Y%m', created_at)").order(created_at: :desc).count
       
