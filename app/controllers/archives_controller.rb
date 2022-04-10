b3a43e0738585c2cf651623fecd5b1a01c0d5678
m_makino <m_makino@upacity.jp>
2022-04-10 23:56:25 +0900

    archives_controllerにpaginate関連の記述を追加

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -3,6 +3,6 @@ class ArchivesController < ApplicationController
     articles = Article.all
     @archives = articles.group("strftime('%Y%m', created_at)").order(created_at: :desc).count
     @yyyymm = params[:yyyymm]
-    @articles = articles.where("strftime('%Y%m', articles.created_at) = '"+@yyyymm+"'").order(created_at: :desc)
+    @articles = articles.where("strftime('%Y%m', articles.created_at) = '"+@yyyymm+"'").page(params[:page]).per(10).order(created_at: :desc)
   end
 end
