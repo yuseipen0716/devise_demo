24b1cd7d334342280e532263dd3268708ef7a8ff
m_makino <m_makino@upacity.jp>
2022-04-10 23:34:24 +0900

    helper methodの中身を少し修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   # archivenのkey(yyyymmdd)とvalue(count)を拾ってきて表示するためのヘルパー
   def show_archive(yyyymm, count)
-    year = yyyymm[0,4]
-    month = yyyymm[4,2]
-     "#{year}年#{month}月(#{count})"
+    yyyy = yyyymm[0,4]
+    mm = yyyymm[4,2]
+    return yyyy + "年" + mm + "月(" + count + ")"
   end
 end
