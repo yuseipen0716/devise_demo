108512f3444398b52156ffa9f2fece85ac9750c3
m_makino <m_makino@upacity.jp>
2022-04-14 22:27:42 +0900

    ActionTextの影響で文字数カウントがおかしくなっていたのでカスタムバリデーション書いた

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,18 @@ class Article < ApplicationRecord
   has_rich_text :body
 
   validates :title, length: { maximum: 100 }, presence: true
-  validates :body, length: { maximum: 100000 }, presence: true
+  validates :body, presence: true
+
+  # ActionTextを利用しており、文字数が従来とずれるためカスタムバリデーションを作成
+  validate :count_as_plaintext
+
+  private
+
+  def count_as_plaintext
+    if body.to_plain_text.gsub(/\n/,"").length > 100000
+      errors.add(:body, 'は100000文字以内で入力してください')
+    end
+  end
 
   # 月別に記事を集計
   #def self.aggregate_monthly
